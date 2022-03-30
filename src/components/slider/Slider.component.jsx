944fd4685afbed15f4398165f78a36b9728da3a9
Amith Sai <amithsai78@gmail.com>
2022-03-30 18:32:59 +0530

    Updated Slider Component

--- src/components/slider/Slider.component.jsx
+++ src/components/slider/Slider.component.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import './Slider.style.css'
-import product from '../../assets/images/product.png'
+import {slider} from './slider.data';
+import { FaArrowLeft, FaArrowRight} from "react-icons/fa";
+
 
 function Slider() {
   return (
-      <div className="slider">
-        <div className="container m-auto flex flex-row items-center px-[23.2rem]">
-            {/* Slider Content */}
-            <div className="slider__content relative">
-                <h1>Die Revolution <br/>der Gewürze</h1>
-                <p>Jeden Tag eine gute Tat. Mit unserem direkten Gewürzhandel kommt mehr Geld bei den Erzeuger*innen im Nahen Osten an und soziale Projekte in der Region erhalten 20% vom Umsatz. Zusammen machen wir den Unterschied!</p>
-                <button className='mt-9'>Jetzt Welt retten!</button>
+    <>
+      {
+        slider.map(({heading, paragraph, image, button, background, size, buttonColor, font}, index) => (
+          <div className="slider" key={index} style={{background: `${background}`}}>
+            <div className={`container m-auto flex flex-row items-stretch px-[23.2rem] relative ${index===0? 'active': ''}`}>
+                {/* Slider Content */}
+                <div className="slider__content flex flex-col justify-center">
+                    <h1 style={{fontSize: `${size}`, fontFamily: `"${font}", sans-serif`, fontWeight: `${font==="Inter" ? '700' : '500'}`}} className={`${index!==0? 'uppercase': ''}`}>{heading.split("\n").map((str, i) => <p key={i}>{str}</p>)}</h1>
+                    <p>{paragraph}</p>
+                    <button className='mt-9' style={{backgroundColor: `${buttonColor}`}}>{button}</button>
+                </div>
+                {/* Slider Images */}
+                <div className="slider__images relative">
+                    <img src={image} alt="Product" />
+                    <div className="indicators flex flex-row items-center jutify-center">
+                      <a href="" className='flex items-center justify-center'><FaArrowLeft /></a>
+                      <a href="" className='flex items-center justify-center'><FaArrowRight /></a>
+                    </div>
+                </div>
             </div>
-            {/* Slider Images */}
-            <div className="slider__images">
-                <img src={product} alt="Product" />
-            </div>
-        </div>
-      </div>
+          </div>
+        ))
+      }
+      
+    </>
   )
 }
 
---
+++ src/components/slider/slider.data.js
@@ -0,0 +1,22 @@
+export const slider = [
+    {
+        heading: "Die Revolution \nder Gewürze",
+        paragraph: "Jeden Tag eine gute Tat. Mit unserem direkten Gewürzhandel kommt mehr Geld bei den Erzeuger*innen im Nahen Osten an und soziale Projekte in der Region erhalten 20% vom Umsatz. Zusammen machen wir den Unterschied!",
+        button: "Jetzt Welt retten!",
+        image: "https://assets-global.website-files.com/5fad213887b737d87d336883/607d236d72150d3d11b17b44_000_group_large.png",
+        background: "linear-gradient(to right, var(--cream-v2) 0%, var(--cream-v2) 59%, var(--black) 59%, var(--black) 100%)",
+        buttonColor: "var(--black)",
+        size: "5.27rem",
+        font: "albra medium"
+    },
+    {
+        heading: "Koriander",
+        paragraph: "Die blumige, frische Koriandersaat ist geschmacklich nicht mit dem Koriandergrün zu vergleichen. Perfekt um jedes deiner Pfannengerichte abzurunden.",
+        button: "Mehr erfahren",
+        image: "https://assets-global.website-files.com/5fad2152fd8977449067ec8e/607891db68f712c04da86f03_008_coriander_large.png",
+        background: "linear-gradient(to right, var(--cream-v2) 0%, var(--cream-v2) 59%, var(--primary) 59%, var(--primary) 100%)",
+        buttonColor: "var(--primary)",
+        size: "6.59rem",
+        font: "Inter"
+    }
+]
