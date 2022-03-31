b89ad14a7fea3a90be9831e9446607749e614540
Amith Sai <amithsai78@gmail.com>
2022-03-31 21:09:36 +0530

    Updated Slider Component

--- src/components/slider/Slider.component.jsx
+++ src/components/slider/Slider.component.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Slider.style.css'
 import {slider} from './slider.data';
 import { FaArrowLeft, FaArrowRight} from "react-icons/fa";
 
 
 function Slider() {
+  const length = slider.length;
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [prevIndex, setPrevIndex] = useState(-1);
+  const [direction, setDirection] = useState('');
+
+  const prevSlide = (event) => {
+    event.preventDefault();
+    setDirection("left");
+    setPrevIndex(currentSlide);
+    setCurrentSlide((currentSlide+1)%length);
+    console.log("CUrrent Slide "+ currentSlide + " Prev Slide "+prevIndex);
+  }
+
+  const nextSlide = (event) => {
+    event.preventDefault();
+    setDirection("right")
+    setPrevIndex(currentSlide)
+    setCurrentSlide((currentSlide==0)? length-1:currentSlide-1);
+    console.log("CUrrent Slide "+ currentSlide + " Prev Slide "+prevIndex);
+  }
+
   return (
     <>
       {
         slider.map(({heading, paragraph, image, button, background, size, buttonColor, font}, index) => (
-          <div className="slider" key={index} style={{background: `${background}`}}>
-            <div className={`container m-auto flex flex-row items-stretch px-[23.2rem] relative ${index===0? 'active': ''}`}>
+          <div className={`slider ${index===currentSlide? 'active': ''}`} key={index} style={{background: `${background}`}}>
+            <div className='container m-auto flex flex-row items-stretch px-[23.2rem] relative'>
                 {/* Slider Content */}
-                <div className="slider__content flex flex-col justify-center">
+                <div className={`slider__content flex flex-col justify-center ${index===currentSlide? 'fadeIn': ''} ${index===prevIndex? 'fadeOut': ''} `}>
                     <h1 style={{fontSize: `${size}`, fontFamily: `"${font}", sans-serif`, fontWeight: `${font==="Inter" ? '700' : '500'}`}} className={`${index!==0? 'uppercase': ''}`}>{heading.split("\n").map((str, i) => <p key={i}>{str}</p>)}</h1>
                     <p>{paragraph}</p>
                     <button className='mt-9' style={{backgroundColor: `${buttonColor}`}}>{button}</button>
                 </div>
                 {/* Slider Images */}
                 <div className="slider__images relative">
-                    <img src={image} alt="Product" />
+                    <img src={image} alt="Product" className={`${index===currentSlide? 'fadeIn': ''} ${index===prevIndex? 'fadeOut': ''} `} />
                     <div className="indicators flex flex-row items-center jutify-center">
-                      <a href="" className='flex items-center justify-center'><FaArrowLeft /></a>
-                      <a href="" className='flex items-center justify-center'><FaArrowRight /></a>
+                      <a href="/" className='flex items-center justify-center' onClick={nextSlide}><FaArrowLeft /></a>
+                      <a href="/" className='flex items-center justify-center' onClick={prevSlide}><FaArrowRight /></a>
                     </div>
                 </div>
             </div>
