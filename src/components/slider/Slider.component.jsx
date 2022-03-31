c0b80d5bc4d4a5168f09fa13f17cfea534dfcbe6
Amith Sai <amithsai78@gmail.com>
2022-03-31 21:35:40 +0530

    Updated Slider Component

--- src/components/slider/Slider.component.jsx
+++ src/components/slider/Slider.component.jsx
@@ -8,22 +8,20 @@ function Slider() {
   const length = slider.length;
   const [currentSlide, setCurrentSlide] = useState(0);
   const [prevIndex, setPrevIndex] = useState(-1);
-  const [direction, setDirection] = useState('');
+  const [direction, setDirection] = useState('right');
 
   const prevSlide = (event) => {
     event.preventDefault();
-    setDirection("left");
+    setDirection('left')
     setPrevIndex(currentSlide);
     setCurrentSlide((currentSlide+1)%length);
-    console.log("CUrrent Slide "+ currentSlide + " Prev Slide "+prevIndex);
   }
 
   const nextSlide = (event) => {
     event.preventDefault();
-    setDirection("right")
+    setDirection('right')
     setPrevIndex(currentSlide)
-    setCurrentSlide((currentSlide==0)? length-1:currentSlide-1);
-    console.log("CUrrent Slide "+ currentSlide + " Prev Slide "+prevIndex);
+    setCurrentSlide((currentSlide===0)? length-1:currentSlide-1);
   }
 
   return (
@@ -33,14 +31,14 @@ function Slider() {
           <div className={`slider ${index===currentSlide? 'active': ''}`} key={index} style={{background: `${background}`}}>
             <div className='container m-auto flex flex-row items-stretch px-[23.2rem] relative'>
                 {/* Slider Content */}
-                <div className={`slider__content flex flex-col justify-center ${index===currentSlide? 'fadeIn': ''} ${index===prevIndex? 'fadeOut': ''} `}>
+                <div className={`slider__content flex flex-col justify-center ${index===currentSlide? direction==='right' ? 'fadeRightIn' : 'fadeLeftIn': ''} ${index===prevIndex? direction==='right' ? 'fadeRightOut' : 'fadeLeftOut': ''} `}>
                     <h1 style={{fontSize: `${size}`, fontFamily: `"${font}", sans-serif`, fontWeight: `${font==="Inter" ? '700' : '500'}`}} className={`${index!==0? 'uppercase': ''}`}>{heading.split("\n").map((str, i) => <p key={i}>{str}</p>)}</h1>
                     <p>{paragraph}</p>
                     <button className='mt-9' style={{backgroundColor: `${buttonColor}`}}>{button}</button>
                 </div>
                 {/* Slider Images */}
                 <div className="slider__images relative">
-                    <img src={image} alt="Product" className={`${index===currentSlide? 'fadeIn': ''} ${index===prevIndex? 'fadeOut': ''} `} />
+                    <img src={image} alt="Product" className={`${index===currentSlide? direction==='right' ? 'fadeRightIn' : 'fadeLeftIn': ''} ${index===prevIndex? direction==='right' ? 'fadeRightOut' : 'fadeLeftOut': ''} `} />
                     <div className="indicators flex flex-row items-center jutify-center">
                       <a href="/" className='flex items-center justify-center' onClick={nextSlide}><FaArrowLeft /></a>
                       <a href="/" className='flex items-center justify-center' onClick={prevSlide}><FaArrowRight /></a>
